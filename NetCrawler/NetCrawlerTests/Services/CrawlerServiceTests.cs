0e1641197e0afedaab6bd700da77be473bf01c94
Peterson souza da costa <Peter.costa@outlook.com>
2021-09-26 01:59:23 -0300

    Added ExecuteTestWithUrl_ShouldFindImagesAndWords unit test.

--- NetCrawler/NetCrawlerTests/Services/CrawlerServiceTests.cs
+++ NetCrawler/NetCrawlerTests/Services/CrawlerServiceTests.cs
@@ -1,8 +1,11 @@
-﻿using Moq;
+﻿using HtmlAgilityPack;
+using Moq;
 using NetCrawler.Models;
 using NUnit.Framework;
 using System;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace NetCrawler.Services.Tests
 {
@@ -14,11 +17,38 @@ namespace NetCrawler.Services.Tests
         {
             //Arrange
             var model = new CrawlerViewModel();
-            var service = new CrawlerService();
+            var httpServiceMock = Mock.Of<IHttpService>();
+            var service = new CrawlerService(httpServiceMock);
 
             //Assert
             var ex = Assert.ThrowsAsync<Exception>(() => service.Execute(model));
             Assert.That(ex.Message, Is.EqualTo("Website's url is required"));
+        }
+
+        [Test]
+        public async Task ExecuteTestWithUrl_ShouldFindImagesAndWords()
+        {
+            //Arrange
+            var expectedImages = 2;
+            var expectedWords = 2;
+            var htmlDocument = new HtmlDocument();
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Services", @"Teste.html");
+            htmlDocument.Load(path);
+            var model = new CrawlerViewModel();
+            model.Url = path;
+            var httpServiceMock = new Mock<IHttpService>();
+            httpServiceMock
+                .Setup(s => s.Execute(path))
+                .ReturnsAsync(htmlDocument);
+
+            var service = new CrawlerService(httpServiceMock.Object);
+
+            //Act
+            await service.Execute(model);
+
+            //Assert
+            Assert.AreEqual(expectedImages, model.Images.Count());
+            Assert.AreEqual(expectedWords, model.CommomWords.Count());
         }
     }
 }
